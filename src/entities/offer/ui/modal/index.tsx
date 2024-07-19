6b1359886bfadd1a7d97c4cba7830d3f609716dc
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-07-19 09:41:40 +0300

    test

--- src/entities/offer/ui/modal/index.tsx
+++ src/entities/offer/ui/modal/index.tsx
@@ -5,11 +5,7 @@ import { ModalLayout } from '@/src/entities/modal';
 export const OfferModal = () => {
   return (
     <ModalLayout footer={<ScanQrOfferButton />}>
-      test <br />
-      test <br />
-      test <br />
-      test <br />
-      test <br />
+      test <br className='ddas' id='da' />
       test <br />
       test <br />
       test <br />
