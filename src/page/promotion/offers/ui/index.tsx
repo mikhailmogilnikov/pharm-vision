80371b5ad8cd17cc3e1293cbe59fb25dbfa1595a
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-07-19 10:30:17 +0300

    test husky1

--- src/page/promotion/offers/ui/index.tsx
+++ src/page/promotion/offers/ui/index.tsx
@@ -7,12 +7,9 @@ type Props = { promotionId: string };
 export const PromotionOffersPage = ({ promotionId }: Props) => {
   return (
     <ul className='grid gap-4 sm:grid-cols-2 animate-appear'>
-
-
+      f
       {OffersMockData.map((offer) => (
-
-        
-        <OfferCard offer={offer}  key={offer.id} />
+        <OfferCard key={offer.id} offer={offer} />
       ))}
     </ul>
   );
