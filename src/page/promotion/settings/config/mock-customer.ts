1121f8bbf78e02c258de8ac6a16d0c3d5dc40029
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-09-05 14:26:50 +0300

    fix: mock customer

--- src/page/promotion/settings/config/mock-customer.ts
+++ src/page/promotion/settings/config/mock-customer.ts
@@ -5,7 +5,6 @@ export const MockCustomer: ICustomer = {
   name: 'Михаил',
   surname: 'Могильников',
   patronymic: 'Андреевич',
-  phone: '+7 (932) 050-54-97',
   email: 'mikhail.mogilnikov02@gmail.com',
   address: 'г. Москва',
 };
